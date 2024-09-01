95bc8be98cf737fe5dd9216a964d14a59d26b493
Muhammad Hassan <158051981+hassan-ca@users.noreply.github.com>
2024-09-01 11:54:59 +0500

    use yarn and remove unused packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "static.vecteezy.com",
-      },
-      {
-        protocol: "https",
-        hostname: "mate-imgs.s3.me-central-1.amazonaws.com",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
